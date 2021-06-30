ef4780cca57d32b759d903421aa0943c57f25992
Gregor Jasny <gregor.jasny@logmein.com>
2021-06-30 08:15:21 +0200

    fixup! [WIP] Add tests for http storage

--- test/suites/secondary_http.bash
+++ test/suites/secondary_http.bash
@@ -1,3 +1,16 @@
+start_http_server() {
+    export CCACHE_SECONDARY_STORAGE="http://127.0.0.1:8080/"
+
+    trap 'terminate_all_children' EXIT # maybe move to global run
+
+    mkdir "secondary"
+    pushd "secondary" >/dev/null
+    echo "=== TEST ${CURRENT_TEST} ===" >> $ABS_TESTDIR/http.log
+    "$HTTP_SERVER" --bind 127.0.0.1 8080 >> $ABS_TESTDIR/http.log 2>&1 &
+    popd >/dev/null
+    sleep 1 # http server startup is slower than test
+}
+
 SUITE_secondary_http_PROBE() {
     if ! "$HTTP_SERVER" --help >/dev/null 2>&1; then
         echo "Cannot execute ($HTTP_SERVER). Python 3 might be missing installed"
@@ -6,17 +19,14 @@ SUITE_secondary_http_PROBE() {
 
 SUITE_secondary_http_SETUP() {
     unset CCACHE_NODIRECT
-    export CCACHE_SECONDARY_STORAGE="http://127.0.0.1:8080/"
 
-    trap 'kill $(jobs -p)' EXIT
-
-    mkdir "secondary"
-    pushd "secondary"
-    "$HTTP_SERVER" --bind 127.0.0.1 8080 > $ABS_TESTDIR/http.log 2>&1 &
-    popd
-    sleep 1 # http server startup is slower than test
+    start_http_server
 
     generate_code 1 test.c
+}
+
+SUITE_secondary_http_TEARDOWN() {
+    terminate_all_children
 }
 
 SUITE_secondary_http() {
@@ -45,41 +55,6 @@ SUITE_secondary_http() {
     expect_stat 'files in cache' 0
     expect_stat 'files in cache' 0
     expect_file_count 2 '*' secondary # result + manifest
-return 0
-    # -------------------------------------------------------------------------
-    TEST "Two directories"
-
-    CCACHE_SECONDARY_STORAGE+=" file://$PWD/secondary_2"
-    mkdir secondary_2
-
-    $CCACHE_COMPILE -c test.c
-    expect_stat 'cache hit (direct)' 0
-    expect_stat 'cache miss' 1
-    expect_stat 'files in cache' 2
-    expect_file_count 3 '*' secondary # CACHEDIR.TAG + result + manifest
-    expect_file_count 3 '*' secondary_2 # CACHEDIR.TAG + result + manifest
-
-    $CCACHE -C >/dev/null
-    expect_stat 'files in cache' 0
-    expect_file_count 3 '*' secondary # CACHEDIR.TAG + result + manifest
-    expect_file_count 3 '*' secondary_2 # CACHEDIR.TAG + result + manifest
-
-    $CCACHE_COMPILE -c test.c
-    expect_stat 'cache hit (direct)' 1
-    expect_stat 'cache miss' 1
-    expect_stat 'files in cache' 0
-    expect_file_count 3 '*' secondary # CACHEDIR.TAG + result + manifest
-    expect_file_count 3 '*' secondary_2 # CACHEDIR.TAG + result + manifest
-
-    rm -r secondary/??
-    expect_file_count 1 '*' secondary # CACHEDIR.TAG
-
-    $CCACHE_COMPILE -c test.c
-    expect_stat 'cache hit (direct)' 2
-    expect_stat 'cache miss' 1
-    expect_stat 'files in cache' 0
-    expect_file_count 1 '*' secondary # CACHEDIR.TAG
-    expect_file_count 3 '*' secondary_2 # CACHEDIR.TAG + result + manifest
 
     # -------------------------------------------------------------------------
     TEST "Read-only"
@@ -88,11 +63,11 @@ return 0
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache miss' 1
     expect_stat 'files in cache' 2
-    expect_file_count 3 '*' secondary # CACHEDIR.TAG + result + manifest
+    expect_file_count 2 '*' secondary # result + manifest
 
     $CCACHE -C >/dev/null
     expect_stat 'files in cache' 0
-    expect_file_count 3 '*' secondary # CACHEDIR.TAG + result + manifest
+    expect_file_count 2 '*' secondary # result + manifest
 
     CCACHE_SECONDARY_STORAGE+="|read-only"
 
@@ -100,28 +75,12 @@ return 0
     expect_stat 'cache hit (direct)' 1
     expect_stat 'cache miss' 1
     expect_stat 'files in cache' 0
-    expect_file_count 3 '*' secondary # CACHEDIR.TAG + result + manifest
+    expect_file_count 2 '*' secondary # result + manifest
 
     echo 'int x;' >> test.c
     $CCACHE_COMPILE -c test.c
     expect_stat 'cache hit (direct)' 1
     expect_stat 'cache miss' 2
     expect_stat 'files in cache' 2
-    expect_file_count 3 '*' secondary # CACHEDIR.TAG + result + manifest
-
-    # -------------------------------------------------------------------------
-    TEST "umask"
-
-    CCACHE_SECONDARY_STORAGE="file://$PWD/secondary|umask=022"
-    rm -rf secondary
-    $CCACHE_COMPILE -c test.c
-    expect_perm secondary drwxr-xr-x
-    expect_perm secondary/CACHEDIR.TAG -rw-r--r--
-
-    CCACHE_SECONDARY_STORAGE="file://$PWD/secondary|umask=000"
-    $CCACHE -C >/dev/null
-    rm -rf secondary
-    $CCACHE_COMPILE -c test.c
-    expect_perm secondary drwxrwxrwx
-    expect_perm secondary/CACHEDIR.TAG -rw-rw-rw-
+    expect_file_count 2 '*' secondary # result + manifest
 }
